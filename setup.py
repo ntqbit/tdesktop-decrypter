e77a0284d2025a9242c89a57b9b9057bf7f32dbb
ntqbit <ntqbit@proton.me>
2025-06-20 14:19:24 +0000

    Add console entry points, allow install with pipx

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(
     name="Telegram Desktop decrypter",
     version="1.1",
     packages=['tdesktop_decrypter'],
-    install_requires=['tgcrypto']
+    install_requires=['tgcrypto'],
+    entry_points={
+        'console_scripts': ['tdesktop-decrypter=tdesktop_decrypter:main']
+    }
 )
